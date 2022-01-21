88255615ab5ba73cfdda49b4a2677d31d2449f1b
Farhan Rehman <37966672+FarhanRehman@users.noreply.github.com>
2022-01-20 21:20:55 -0500

    heroku debug

--- api/api/settings.py
+++ api/api/settings.py
@@ -12,13 +12,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Activate Django-Heroku.
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
